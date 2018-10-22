c2b95542fd3bd3eee620591c05c645991ae93c95
Shuangquan Li <shuangquan_li@icloud.com>
2018-10-22 14:20:45 -0500

    Initial Commit

---
+++ xocde-github/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xocde-github
+//
+//  Created by 李双全 on 10/22/18.
+//  Copyright © 2018 Shuangquan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
